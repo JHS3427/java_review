5fd10d721d5769d5cc66fc9cc7bd2330d2311faf
JHS3427 <jhs34276225@naver.com>
2025-06-27 18:18:50 +0900

    5th days of study

--- src/chapter05/ForMountain.java
+++ src/chapter05/ForMountain.java
@@ -42,5 +42,22 @@ public class ForMountain {
 				}
 			}
 		}
-	}
+		
+		int evenSum = 0;
+		int oddSum = 0;
+		for (int i = 1 ; i<=100 ; i++)
+		{
+			if(i%2==0)
+			{
+				evenSum+=i;
+			}
+			else
+			{
+				oddSum+=i;
+			}
+		}
+		System.out.println("evenSum : " + evenSum);
+		System.out.println("oddSum : " + oddSum);
+		
+	}//main method
 }
